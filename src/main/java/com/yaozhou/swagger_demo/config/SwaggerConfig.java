a0453ed6b4c6f3de018c8c1a7ad71d0ff8dfbee1
yaozhoucn <wangxiao_h@126.com>
2021-09-18 15:44:52 +0800

    swaggerConfig

--- src/main/java/com/yaozhou/swagger_demo/config/SwaggerConfig.java
+++ src/main/java/com/yaozhou/swagger_demo/config/SwaggerConfig.java
@@ -1,7 +1,14 @@
 package com.yaozhou.swagger_demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * Created by WXHang on HANG at 2021/9/18 14:59
@@ -10,4 +17,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2  //开启swagger2
 public class SwaggerConfig {
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }
+    public ApiInfo apiInfo(){
+        Contact contact = new Contact("yaozhou","127.0.0.1","3124723678@qq.com");
+        return new ApiInfo(
+                "YaoShen Documentation",
+                "Api Documentation",
+                "1.0",
+                "urn:tos",
+                contact,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+
+        );
+    }
 }
